a1b0ff395d38f0170b51eff27b4a5cffb55dbb85
johncook <jonnymcook1@gmail.com>
2019-07-16 17:33:14 -0500

    login and register

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -17,6 +17,7 @@ class Login extends Component {
     }
 
     login = () => {
+        console.log(this.state.username)
         let {username, password} = this.state
         axios
         .post('/login', {username: username, password: password})
@@ -33,8 +34,8 @@ class Login extends Component {
                 <div>
                     <h2>Log In To Alpha</h2>
                     <h4> All fields are required</h4>
-                    <input className='login-input' placeholder='Username' name='username'></input>
-                    <input className='password-input' placeholder='Password' name='password'></input>
+                    <input className='login-input' placeholder='Username' name='username' value={this.state.username} onChange={this.handleChange}></input>
+                    <input className='password-input' placeholder='Password' name='password' value={this.state.password} onChange={this.handleChange}></input>
                     <button onClick={this.login}> Login</button>
                 </div>
             </div>
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -23,18 +23,17 @@ class SignUp extends Component {
             miles: 0,
             redirect: false
         }
-        this.signupMember = this.signupMember.bind(this)
+        // this.signupMember = this.signupMember.bind(this)
     }
 
     handleChange = (e) => {
         this.setState({[e.target.name]: e.target.value})
       }
     
-
-
-    signupMember() {
+    signupMember = (e) => {
+        e.preventDefault()
         // console.log(this.state)
-        axios.post("/register", { first_name: this.state.first_name, last_name: this.state.last_name, gender: this.state.gender, birthdate: this.state.birthdate,
+        axios.post('/register', { first_name: this.state.first_name, last_name: this.state.last_name, gender: this.state.gender, birthdate: this.state.birthdate,
             address: this.state.address, address2: this.state.address2, city: this.state.city, state: this.state.state, zip: this.state.zip, phone: this.state.phone,
             email: this.state.email, username: this.state.username, password: this.state.password})
         .then(() => this.setState({redirect: true})).catch((err) => alert(err, 'Username Taken'))
@@ -43,7 +42,7 @@ class SignUp extends Component {
     render() {
         if(this.state.redirect){
             alert("Please Log In")
-            return<Redirect to="/login" />
+            return<Redirect to="/auth/login" />
         }
         return (
             <div>
@@ -51,19 +50,19 @@ class SignUp extends Component {
            <h3>Create a New Account</h3>
         
         <div>
-            <input name='first_name' onChange={this.handleFirst_name} placeholder="firstName" />
-            <input name='last_name' onChange={this.handleLast_name} placeholder="lastName" />
-            <input name='gender' onChange={this.handleGender} placeholder="gender"/>
-            <input name='birthdate' onChange={this.handleBirthdate} placeholder="birthdate"/>
-            <input name='address' onChange={this.handleAddress} placeholder="address"/>
-            <input name='address2' onChange={this.handleAddress2} placeholder="address2"/>
-            <input name='city' onChange={this.handleCity} placeholder="city"/>
-            <input name='state' onChange={this.handleState} placeholder="state"/>
-            <input name='zip' onChange={this.handleZip} placeholder="zip"/>
-            <input name='phone' onChange={this.handlePhone} placeholder="phone"/>
-            <input name='email' onChange={this.handleEmail} placeholder="email@email.com"/>
-            <input name='username' onChange={this.handleUsername} placeholder="username"/>
-            <input name='password' onChange={this.handlePassword}placeholder="password" type="password"/>
+            <input name='first_name' value={this.state.first_name} onChange={this.handleChange} placeholder="firstName" />
+            <input name='last_name' value={this.state.last_name} onChange={this.handleChange} placeholder="lastName" />
+            <input name='gender' value={this.state.gender} onChange={this.handleChange} placeholder="gender"/>
+            <input name='birthdate' value={this.state.birthdate} onChange={this.handleChange} placeholder="birthdate"/>
+            <input name='address' value={this.state.address} onChange={this.handleChange} placeholder="address"/>
+            <input name='address2' value={this.state.address2} onChange={this.handleChange} placeholder="address2"/>
+            <input name='city' value={this.state.city} onChange={this.handleChange} placeholder="city"/>
+            <input name='state' value={this.state.state} onChange={this.handleChange} placeholder="state"/>
+            <input name='zip' value={this.state.zip} onChange={this.handleChange} placeholder="zip"/>
+            <input name='phone' value={this.state.phone} onChange={this.handleChange} placeholder="phone"/>
+            <input name='email' value={this.state.email} onChange={this.handleChange} placeholder="email@email.com"/>
+            <input name='username' value={this.state.username} onChange={this.handleChange} placeholder="username"/>
+            <input name='password' value={this.state.password} onChange={this.handleChange}placeholder="password" type="password"/>
         </div>
         <button className="signup-member" onClick={this.signupMember}>Add New Account</button>
         </div>
