de5ffbb68b036786c46a4e74c41eda7c64008935
donna <sheehanqmd@gmail.com>
2019-07-17 12:07:44 -0500

    add results

--- src/components/Nav/Book/Book.js
+++ src/components/Nav/Book/Book.js
@@ -7,7 +7,7 @@ class Book extends Component {
     constructor() {
         super();
         this.state = {
-
+        
         }
     }
 
--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,11 +1,36 @@
 import React, {Component} from 'react';
 
 class Results extends Component {
+    constructor() {
+        super();
+        this.state = {
+            flightNumber: 0,
+            departureCity:"",
+            departureTime:"",
+            arrivalCity: "",
+            arrivalTime: "",
+            duration: "",
+            aircraftType: "",
+            ticketCost: ""
+        }
+    }
+
+
+
     render() {
         return (
             <h1>Results</h1>
         )
     }
 }
+
+// const mapStateToProps = reduxState => {
+//     const {member} = reduxState ;
+//     return {
+//         member
+//     }
+// }
+
+// export default connect ( mapStateToProps, {getMember})
 
 export default Results;
