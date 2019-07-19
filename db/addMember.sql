0df29b32d5dc2528b68ca64a6b98cf2236eac12c
johncook <jonnymcook1@gmail.com>
2019-07-19 10:59:31 -0500

    add addMember

--- db/addMember.sql
+++ db/addMember.sql
@@ -1,3 +1,3 @@
-INSERT INTO members (first_name, last_name, gender, birthdate, address, address2, city, state, zip, phone, email, username, hash)
-VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)
+INSERT INTO members (first_name, last_name, gender, DOBmonth, DOByear, DOBday, address, address2, city, state, zip, phone, email, username, hash)
+VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15)
 RETURNING *;
