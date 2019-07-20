6e6ed165057b1f813d2a211a5159a903edc47837
johncook <jonnymcook1@gmail.com>
2019-07-20 09:36:42 -0500

    add flightController

--- server/controllers/flightController.js
+++ server/controllers/flightController.js
@@ -1,3 +1,8 @@
 module.exports = {
-    
+    addToCheckout: (req, res) => {
+        console.log("hit")
+        req.session.push(req.body)
+        req.session.total += req.body.amount
+        res.status(200).json(req.session.checkout)
+    },
 }
