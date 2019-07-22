bf8f015d08bdb51e166dfda6ca759a217361f9fe
donna <sheehanqmd@gmail.com>
2019-07-22 09:36:27 -0500

    add tests

--- src/App.test.js
+++ src/App.test.js
@@ -7,3 +7,30 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+test("Results Test", () => {
+  let results = [flightNumber,
+  departureCity,
+  departureTime,
+  arrivalCity,
+  arrivalTime,
+  duration ,
+  aircraftType,
+  ticketCost]; 
+  expected(results).toContain("flightNumber");
+});
+
+ 
+test("Results Test", () => {
+const h = Math.floor(minutes / 60);
+expect(120).toMatchSnapshot(2);
+});
+
+test("Results Test", () => {
+const m = minutes % 60;
+expect(120).toMatchSnapshot(2)
+});
+
+test("Results Test", () => {
+const data = someFunctionYouAreTesting()
+expect(data).toMatchSnapshot()
+});
