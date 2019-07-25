27265779926b237dc5caaca0441c4e1ab1255fa0
donna <sheehanqmd@gmail.com>
2019-07-24 22:26:41 -0500

    add render up coming flights

--- src/components/Dashboard/Upcoming.js
+++ src/components/Dashboard/Upcoming.js
@@ -8,9 +8,49 @@ class Upcoming extends Component {
         super(props) 
         this.state = {
             flights: []
+            
         }
     }
-        
+   
+
+   
+    changeDate = (str) => {
+        let newStr = str.split('')
+        newStr.splice(0, 12)
+        let mins = newStr.join('')
+
+        return mins
+    }
+    changeTime = (time) => {
+        let hours = parseInt(time.substring(11, 13))
+        let mins = time.substring(14, 16)
+
+        if (hours == '00') hours = 12;
+        if (hours > '12') hours = hours - 12;
+
+        return hours + ':' + mins
+    }
+
+    getAM = (time) => {
+        let firstTime = time.split('')
+        let middleTime = firstTime.splice(11, 5)
+        let finalTime = middleTime.join('')
+        let militaryTime = finalTime.split(':')
+        let endTime = (militaryTime[0].charAt(0) == 1 && militaryTime[0].charAt(1) > 2) ? (+militaryTime[0] - 12) + ':' + militaryTime[1] + ' PM' : militaryTime[0].charAt(0) == 1 && militaryTime[0].charAt(1) == 2 ? militaryTime[0] + ':' + militaryTime[1] + ' PM' : militaryTime[0].charAt(0) == 0 && militaryTime[0].charAt(1) == 0 ? 12 + ':' + militaryTime[1] + ' AM' : militaryTime.join(':') + ' AM'
+      
+        let splitFinal = endTime.split('')
+      
+        let splitMiddle = splitFinal.slice(1).slice(-2)
+      
+        let finalFinal = splitMiddle.join('')
+      
+        return finalFinal
+    }
+
+
+      
+
+
     componentDidMount() {
         this.props.getFlight()
     }
@@ -40,23 +80,24 @@ console.log(this.props.flight)
                                    {flight && flight.map((flight, index) => 
                                      { console.log(flight)
                                      return (<div className='upcoming-info'>
-                                     <div className='next'>
+                                     {/* <div className='next'>
                                          <h4>UP NEXT</h4>
                                          <h2>{flight.departure_time}</h2>
-                                     </div>
+                                     </div> */}
                
                                      <div className='flight-info'>
                                          <div className='outbound'>
                                              <h4>Outbound</h4>
                                              
-                                             <h2>{flight.departure_time}</h2>
+                                             <h2>{this.changeDate(flight.departure_time)}</h2>
                                          </div>
   
                                          <div>
                                              <h4>Departs</h4>
                                              <h1>{flight.departure_city}</h1>
                                              <div className='flight-time'>
-                                                 <h3>{flight.departure_time}</h3>
+                                                 <h3>{this.changeTime(flight.departure_time)}</h3>
+                                                 <h5>{this.getAM(flight.departure_time)}</h5>
                                                 
                                              </div>
                                          </div>
@@ -67,7 +108,9 @@ console.log(this.props.flight)
                                              <h4>Arrives</h4>
                                              <h1>{flight.arrival_city}</h1>
                                              <div className='flight-time'>
-                                                 <h3>{flight.arrival_time}</h3>
+                                             <h3>{this.changeTime(flight.arrival_time)}</h3>
+                                                 <h5>{this.getAM(flight.arrival_time)}</h5>
+                                                 
                                                 
                                              </div>
                                          </div>
@@ -93,6 +136,7 @@ console.log(this.props.flight)
 }
 
 }
+
 
 
 
